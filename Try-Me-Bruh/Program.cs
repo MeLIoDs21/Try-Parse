a5a75c52d89d3f952b6b2f57c342c5882c8d7041
MeLIoDs21 <loal00001@utb.vaxjo.se>
2020-11-17 10:41:55 +0100

    Add files via upload

---
+++ Try-Me-Bruh/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Try_Me_Bruh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a whole number: ");
+            int? a;
+            TryParse(Console.ReadLine(), out a);
+            Console.WriteLine(a);
+            System.Threading.Thread.Sleep(3000);
+        }
+
+
+        public static bool TryParse(string _A, out int? _Number)
+        {
+            try
+            {
+                _Number = Convert.ToInt32(_A);
+                return true;
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("Felaktig inmättning! Använd bara Heltal.");
+                _Number = null;
+                return false;
+            }
+            catch(OverflowException)
+            {
+                Console.WriteLine("Felaktig inmättning! Talet är för stort.");
+                _Number = null;
+                return false;
+            }
+
+
+
+
+        }
+    }
+}
